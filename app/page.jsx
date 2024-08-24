09853c56efbdf36fbb9ce2031f28b3de21f45088
Laurent Gagné <laurentgagne@Laurents-MacBook-Air.local>
2024-08-25 00:24:30 +0200

    trying 2

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import { FiDownload } from "react-icons/fi";
 
 //components
 import Social from "@/components/Social";
-import Photo from "@/components/photo";
+import Photo from "@/components/Photo";
 import Stats from "@/components/Stats";
 
 const Home = () => {
